8264594f6dec1a7b142308b4a1f784d4c5d2da6b
Qiugu He <kaochi545121151@gmail.com>
2019-05-30 11:49:44 -0500

    Start sharing: -

---
+++ app/src/main/java/com/qiu/uber/DriverLoginActivity.java
@@ -0,0 +1,91 @@
+package com.qiu.uber;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class DriverLoginActivity extends AppCompatActivity {
+    private EditText mEmail, mPassport;
+    private Button mLogin, mRegistration;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener  firebaseAuthLister; //lister
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_login);
+
+        mAuth = FirebaseAuth.getInstance(); //get the current state of login data
+        firebaseAuthLister = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //check the user states
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if(user != null){
+                    Intent intent =  new Intent(DriverLoginActivity.this, .class);
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
+            }
+        };
+
+        mLogin = (Button) findViewById(R.id.login);
+        mRegistration = (Button) findViewById(R.id.registration);
+
+        mRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String email = mEmail.getText().toString();
+                final String passport = mPassport.getText().toString();
+
+                //now use mAuth inorder for user login
+                mAuth.createUserWithEmailAndPassword(email, passport).addOnCompleteListener(DriverLoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            //tell user something wrong
+                            Toast.makeText(DriverLoginActivity.this, "Sign up error", Toast.LENGTH_SHORT).show();
+                        }else{
+                            String user_id = mAuth.getCurrentUser().getUid();
+                            //point to rider
+                            DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child("Driver").child(user_id);
+                            //make reference safe
+                            current_user_db.setValue(true);
+                        }
+                    }
+                });
+
+            }
+        });
+
+        mEmail = (EditText) findViewById(R.id.email);
+        mPassport = (EditText) findViewById(R.id.passport);
+    }
+
+    //when ever the activity id call, we must start the lisenler
+    @Override
+    protected void onStart(){
+        super.onStart();
+        mAuth.addAuthStateListener(firebaseAuthLister);
+    }
+    @Override
+    protected void onStop(){
+        super.onStop();
+        mAuth.removeAuthStateListener(firebaseAuthLister);
+    }
+
+}
